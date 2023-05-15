54a2c48463723db5efa07ceb8cc74400a23adac8
Peter <petersolomon704@gmail.com>
2023-05-15 13:41:02 +0100

    author list
    rendered list

--- src/components/authors/AuthorList.jsx
+++ src/components/authors/AuthorList.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "../authors/Author.css";
+
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`;
 
 const AuthorList = () => {
-  return <div>Author</div>;
+  const { loading, error, data } = useQuery(ALL_AUTHORS);
+  console.log("DATA", data);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="container">
+      <h2 className="author-list-title">author List</h2>
+      <div className="author-hero">
+        {data.allAuthors &&
+          data.allAuthors.map((author) => (
+            <div key={author.id} className="author-wrap">
+              <div className="author">{author.name}</div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default AuthorList;
