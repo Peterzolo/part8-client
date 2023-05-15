ead7b647c4c06fa3bf4440b20c78cd5b72e4fd30
Peter <petersolomon704@gmail.com>
2023-05-15 12:28:53 +0100

    Update Menu.jsx
    Modified code

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <ul className="menu-wrap">
+        <li className="menu-item">Books</li>
+        <li className="menu-item">Authors</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
