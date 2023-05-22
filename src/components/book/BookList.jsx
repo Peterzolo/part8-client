dc972513bae0c4ebd61ee9e6fe00d3f882282167
Peter <petersolomon704@gmail.com>
2023-05-22 14:41:31 +0100

    Update BookList.jsx
    Modified code

--- src/components/book/BookList.jsx
+++ src/components/book/BookList.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import "../book/BookList.css";
 import { useNavigate } from "react-router-dom";
 
 const ALL_BOOKS = gql`
-  query GetAllBooks($genre: String) {
-    getAllBooks(filter: { genre: $genre }) {
+  query GetAllBooks {
+    getAllBooks {
       title
       published
       genre
@@ -19,11 +19,10 @@ const ALL_BOOKS = gql`
 `;
 
 const BookList = () => {
-  const [genreFilter, setGenreFilter] = useState("");
-  const { loading, error, data, refetch } = useQuery(ALL_BOOKS);
+  const { loading, error, data } = useQuery(ALL_BOOKS);
   console.log("DATA", data);
   const navigate = useNavigate();
-  const books = data?.getAllBooks || [];
+  const books = data?.getAllBooks;
 
   if (loading) {
     return <div>Loading...</div>;
@@ -33,50 +32,33 @@ const BookList = () => {
     return <div>Error: {error.message}</div>;
   }
 
-  const handleFilterChange = (event) => {
-    setGenreFilter(event.target.value);
-  };
-
-  const handleFilterSubmit = (event) => {
-    event.preventDefault();
-    refetch({ genre: genreFilter });
-  };
-
   return (
     <div className="container">
-      <form onSubmit={handleFilterSubmit}>
-        <input
-          type="text"
-          placeholder="Filter by genre"
-          value={genreFilter}
-          onChange={handleFilterChange}
-        />
-        <button type="submit">Filter</button>
-      </form>
       <h2 className="book-list-title">Book List</h2>
       <div className="book-hero">
-        {books.map((book) => (
-          <div key={book.id} className="book-wrap">
-            <div className="all-wrap">
-              <h5>Title</h5>
-              <div className="" onClick={() => navigate(`/${book.id}`)}>
-                {book.title}
+        {books &&
+          books?.map((book) => (
+            <div key={book.id} className="book-wrap">
+              <div className="all-wrap">
+                <h5>books</h5>
+                <div className="" onClick={() => navigate(`/${book.id}`)}>
+                  {book.title}
+                </div>
+              </div>
+              <div className="all-wrap">
+                <h5>Published</h5>
+                <div>{book.published}</div>
+              </div>
+              <div className="all-wrap">
+                <h5>Genre</h5>
+                <div>{book.genre}</div>
+              </div>
+              <div className="all-wrap">
+                <h5>Author</h5>
+                <div>{book.author.name}</div>
               </div>
             </div>
-            <div className="all-wrap">
-              <h5>Published</h5>
-              <div>{book.published}</div>
-            </div>
-            <div className="all-wrap">
-              <h5>Genre</h5>
-              <div>{book.genre}</div>
-            </div>
-            <div className="all-wrap">
-              <h5>Author</h5>
-              <div>{book.author.name}</div>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
